cbb17ac176562fddcb0e7eac0abd74885fb89c56
Gokulakannan V <104374973+Gokulakannan-2112@users.noreply.github.com>
2024-01-01 21:08:21 +0530

    Create LeaseNotFoundException.java

---
+++ Exception/LeaseNotFoundException.java
@@ -0,0 +1,19 @@
+package com.car_rental.exception;
+
+public class LeaseNotFoundException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public LeaseNotFoundException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public LeaseNotFoundException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}
